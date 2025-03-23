ae450382f82026f0a4f54503bf2300867afff878
Rishav-Sisodiya <rishavsisodiya2002@gmail.com>
2025-03-23 17:45:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// var tablinks = document.getElementsByClassName("tab-links");
+// var tabcontent = document.getElementsByClassName("tab-content");
+
+// function opentab(tabname){
+//   for(tablink of tablinks){
+//     tablink.classList.remove("active-tab");
+//   }
+//    document.getElementById(tabname).classList.add("active-tab");
+// }
+
+
+function opentab(){
+  console.log("123");
+  document.querySelector('.active-tab').remove();
+
+}
